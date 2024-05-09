96b56e462413746ec5361a48051775e778a1c0bc
Joao Victor <jv-amaralds@hotmail.com>
2024-05-09 09:11:07 -0300

    insercao de usuario com POST

--- src/main/java/com/jvamaral/workshopmongo/services/UserService.java
+++ src/main/java/com/jvamaral/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jvamaral.workshopmongo.domain.User;
+import com.jvamaral.workshopmongo.dto.UserDTO;
 import com.jvamaral.workshopmongo.repositories.UserRepository;
 import com.jvamaral.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,14 @@ public class UserService {
 	public User findById(String id) {
 		User user = repository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto nao encontrado"));
 		return user;
+	}
+	
+	@Transactional
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
