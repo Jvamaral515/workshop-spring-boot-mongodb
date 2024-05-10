e1d83cd9dc1e0bf9be1db17b63160b5be96782fd
Joao Victor <jv-amaralds@hotmail.com>
2024-05-10 08:24:23 -0300

    Obtendo post por id

---
+++ src/main/java/com/jvamaral/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.jvamaral.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jvamaral.workshopmongo.domain.Post;
+import com.jvamaral.workshopmongo.repositories.PostRepository;
+import com.jvamaral.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Post findById(String id) {
+		Post post = repository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto nao encontrado"));
+		return post;
+	}
+}
